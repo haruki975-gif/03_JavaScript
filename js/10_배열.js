3df2069be62ebb1d777417526ce39240b572ec16
haruki975-gif <haruki975@gmail.com>
2025-01-08 15:06:57 +0900

    20250108_요소접근방법

--- js/10_배열.js
+++ js/10_배열.js
@@ -121,3 +121,212 @@ function check4(){
   console.log("짝수 합 : ", evenSum);
   console.log("홀수 합 : ", oddSum);
 }
+
+/** 배열 X for문 확인 4 */
+/* 
+  배열 길이를 입력 받아 배열을 생성하고
+  생성된 배열 각 요소에 0에서 배열 길이 미만의 난수를 저장
+*/
+// math.floor
+function check5(){
+  const len = Number(prompt("생성할 배열 길이 입력"));
+  const arr = new Array(len); // 입력 받은 수 만큼의 칸을 가지는 배열 생성
+  
+  // arr.length === len
+  for(let i=0; i<len; i++){
+    
+    // 0이상 len 미만 난수 발생
+    const num = Math.floor(Math.random() * len);
+    arr[i] = num; // 난수를 배열에 대입
+  }
+  console.log("결과 : ", arr);
+}
+
+
+/** 점심 메뉴 뽑기 */
+function selectMenu(){
+  const menus = [
+    '김밥', '돈까스', '쫄면', '된장찌개', 
+    '칼국수', '쌀국수', '카레', '닭갈비', '샌드위치',
+    '샐러드', '라면', '제육덮밥', '짬뽕', '고등어 구이',
+    '삼겹살', '백반', '우육면', '곰탕', '부대찌개', '텐동'
+  ];
+  
+  // 0 이상 메뉴 개수 미만의 난수 1개 생성
+  const num = Math.floor(Math.random() * menus.length);
+  
+  // id가 menuResult인 요소에 내용으로 출력
+  document.getElementById("menuResult").innerText = menus[num]; 
+}
+
+/** 로또 번호 생성기 */
+/* 
+  배열.includes(값) : () 내 값이 배열에 존재하면 true, 없으면 false
+*/
+/* 
+  - 1 ~ 45 번까지 존재
+  - 6개 뽑기
+  - 중복 X
+*/
+function createLottoNumber(){
+  // id가 lotto인 요소 얻어와 변수에 저장
+  const lotto = document.getElementById("lotto");
+  
+  // 기존에 생성되어있는 번호 삭제("" 빈칸으로 변경)
+  lotto.innerHTML = "";
+  
+  /* 배열에 중복되지 않는 난수 6개 저장 */
+  const arr = [];
+  for(let i=0; i<6; i++){
+    
+    // 1~45 사이 난수 생성
+    const num = Math.floor(Math.random() * 45 + 1);
+    
+    // 만약에 난수 num이 이미 배열에 존재하는 경우
+    if(arr.includes(num)){
+      i--; // index를 뒤로 갔다가 앞으로 오게 함(제자리 유지, 중복으로 빈값 해결)
+      continue; // 다음 반복으로 넘어감
+    }
+    
+    // 중복되지 않는 난수인 경우 배열에 추가
+    arr[i] = num;
+  }
+  
+  // 배열 내 숫자 정렬(sort)
+  arr.sort( function(a,b){ return a-b; } ); // 오름차순(숫자가 커짐) 정렬
+  
+  console.log("arr : ", arr);
+  
+  // 화면에 로또 번호 순서대로 출력하기
+  for(let i=0; i<arr.length; i++){
+    lotto.innerHTML += `<div>${arr[i]}</div>`;
+  }
+}
+
+/** 2차원 배열 확인 1 */
+function check6(){
+  // 변수 선언
+  const num1 = 10;
+  const num2 = 20;
+  const num3 = 30;
+  
+  // 1차원 배열 선언
+  const arr1 = [num1, num2, num3]; // [10, 20, 30]
+  const arr2 = [40 ,50 ,60];
+  const arr3 = [70 ,80 ,90];
+  
+  // 2차원 배열 선언
+  const dArr1 = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+  ];
+  
+  const dArr2 = [arr1, arr2, arr3]; // 1차원 배열 묶음
+  
+  console.log("dArr1 : ", dArr1);
+  console.log("dArr2 : ", dArr2);
+  
+  // -----------------------------
+  
+  // 2차원 배열의 인덱스를 이용해서 접근하기
+  
+  // 1차원 배열 내에 저장된 배열 요소의 개수
+  console.log("dArr2[0] : ", dArr2[0]);
+  console.log("dArr2[0].length : ", dArr2[0].length);
+  
+  console.log("dArr2[1] : ", dArr2[1]);
+  console.log("dArr2[1].length : ", dArr2[1].length);
+  
+  console.log("dArr2[2] : ", dArr2[2]);
+  console.log("dArr2[2].length : ", dArr2[2].length);
+  
+  // 2차원 배열 내에 저장된 1차원 배열 요소의 개수
+  console.log("dArr2.length : ", dArr2.length); // 3
+  
+  // dArr2의 0행 2열에 저장된 값 얻어오기
+  console.log("dArr2[0][2] : ", dArr2[0][2]); // 30
+  
+  // dArr2의 1행 1열에 저장된 값 얻어오기
+  console.log("dArr2[1][1] : ", dArr2[1][1]); // 50
+  
+  // dArr2의 2행 0열에 저장된 값 얻어오기
+  console.log("dArr2[2][0] : ", dArr2[2][0]); // 70
+  
+  
+  /* 2차원 배열 인덱스를 이용해서 값 대입 */
+  
+  // 0행 0열, 1행 0열, 2행 0열 값을 * 10 해서 대입
+  dArr2[0][0] = dArr2[0][0] * 10; // 100
+  dArr2[1][0] *= 10; // 400 복합 대입 연산자
+  dArr2[2][0] *= 10; // 700 복합 대입 연산자
+  
+  console.log(dArr2); // 확인
+}
+
+
+/** 2차원 배열 확인 2 */
+/* 
+  4행 4열짜리 2차원 배열에 1~16 사이 난수 배치 (중복X)
+  1)  배열을 콘솔에 출력
+  2)  prompt를 이용해서 1~16 사이 숫자를 입력 받아
+      2차원 배열 어디에 위치하는지 검색해서 출력하기
+*/
+function check7(){
+  // 중복되지 않는 난수 16개를 저장한 1차원 배열 생성
+  const randomArr = [];
+  for(let i=0; i<16; i++){
+    
+    // 1~16 사이 난수
+    const num = Math.floor(Math.random() * 16+1);
+    
+    if(randomArr.includes(num)){ // 중복이 있을 경우
+      i--;
+      continue;
+    }
+    // 중복x
+    randomArr[i] = num;
+  }
+  console.log("randomArr : ", randomArr);
+
+  
+  // * 2중 for문을 이용해서 2차원 배열 모든 요소 접근하기
+  let index = 0; // randomArr의 요소를 접근하기 위한 변수 선언
+  const arr = []; // 2차원 배열 생성
+  
+  for(let row=0; row<4; row++){ // 행 반복
+    
+    arr[row] = []; // 행 생성(1차원 배열)
+    
+    for(let col=0; col<4; col++){ // 열 반복
+      // arr[row][col] = `(${row}, ${col})`; // 열에 데이터 추가
+      
+      // randomArr 배열의 요소를 하나씩 꺼내서
+      // arr[row][col]에 대입
+      arr[row][col] = randomArr[index];
+      index++; // index 1 증가시켜 randomArr 다음 요소 지정하기
+      
+      // arr[row][col] = randomArr[index++];  // index++ 후위연산
+    }
+  }
+  console.log(arr);
+  
+
+  // 2)  prompt를 이용해서 1~16 사이 숫자를 입력 받아
+  // (정상 입력했다고 가정)
+  const input = Number(prompt("1~16 사이 숫자 입력"));
+  
+  // 2차원 배열 어디에 위치하는지 검색해서 출력하기
+  for(let row = 0; row < arr.length; row++){ // 2차원 배열의 행 반복 (4)
+    for(let col = 0; col < arr[row].length; col++){ // 열 반복 (4)
+      
+      // arr[row][col]에 저장된 값과 입력값(input)이 같을 때
+      if(arr[row][col] === input){
+        alert(`${input}은/는 (${row}, ${col})에 있습니다.`);
+        return; // 값을 찾으면 함수를 종료
+      }
+    }
+  }
+  
+  
+}
